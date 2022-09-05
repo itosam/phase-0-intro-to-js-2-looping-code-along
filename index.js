7da151d16e26ecb0e64a077d99d78d65bb0429c1
Osamu Ito <mr.samito@gmail.com>
2022-09-05 14:30:37 -0400

    commit tree 75f9655883a9d38fa4dd223132159886829991bd

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-// Code your solutions in this file
+// for (let age = 30; age < 40; age ++){
+  //age = the initialization/starting point
+  //age < 40 => the condition "if age is less than 40, then continue"
+  //age ++ is the iteration/what we want done to age in the loop when condition is met.
+
+//   console.log(`I'm ${age} years old. Happy birthday to me!`)
+// }
+
+// const gifts = ['teddy bear', 'drone', 'doll'];
+// function wrapGift(gifts){
+//   for(let i = 1; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+
+//   }
+//   return gifts;
+// }
+
+//wrapGift(gifts);
+
+// function writeCards(arrayNames, event) {
+//   let cards = []
+//   for ( let i = 0; i < arrayNames.length; i++) {
+//     cards.push(`Thank you, ${arrayNames[i]}, for the wonderful ${event} gift!`)
+    
+//     return cards
+//   }
+// }
+
+function writeCards( namesArray, event ) {
+  let thankYouCards = []
+  for ( let i = 0; i < namesArray.length; i++ ) {
+    thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+  }
+  return thankYouCards
+}
+
+// function countUp() {
+
+// let countup = 0;
+// while (countup < 10) {
+//   console.log(countup++);
+// }
+// }
+
+function countDown(number) {
+ while(number > 0) {
+  console.log(number);
+  number -= 1;
+ }
+ console.log(number);
+}
